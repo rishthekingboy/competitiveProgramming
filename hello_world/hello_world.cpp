0e89eee2c9b1daa77ea29fc9921f8088a7a7727a
Mr. Hacker <rish@localhost.localdomain>
2021-02-02 17:17:59 +0530

    Hello World in basics
    adding Hello World src file to basics branch

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std; //standard namespace
+
+int main(){
+
+	cout<< "Hello World" << endl; //getting output
+	
+	return 0; // returning int from int function
+
+}
